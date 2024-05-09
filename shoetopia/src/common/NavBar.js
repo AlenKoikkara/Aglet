a8533835dd7e8bd39365566dfc3392718502cf5d
Alen Koikkara <74101736+Acoderio1@users.noreply.github.com>
2024-05-09 14:59:51 -0500

    mobile resp finished

--- shoetopia/src/common/NavBar.js
+++ shoetopia/src/common/NavBar.js
@@ -1,43 +1,58 @@
-import {React, useState, useEffect} from 'react'
+import { React, useState, useEffect } from "react";
 
-import './NavBar.scss'
+import "./NavBar.scss";
 
-import logo from '../assets/images/logo.png';
-import profilepic from  '../assets/images/profilepic.webp'
-import HamburgerMenu from './HamburgerMenu';
-import utils from '../utils';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import logo from "../assets/images/logo.png";
+import profilepic from "../assets/images/profilepic.webp";
+import HamburgerMenu from "./HamburgerMenu";
+import utils from "../utils";
 
 const NavBar = () => {
-  const [show, handleShow] = useState(true)
+  const [show, handleShow] = useState(true);
 
   const navbarAnimation = () => {
     if (window.scrollY > 100) {
-      handleShow(true)
+      handleShow(true);
     } else {
-      handleShow(false)
+      handleShow(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", navbarAnimation)
-  
-    return () => window.removeEventListener("scroll", navbarAnimation)
-  }, [])
-  
+    window.addEventListener("scroll", navbarAnimation);
+
+    return () => window.removeEventListener("scroll", navbarAnimation);
+  }, []);
+
   return (
     <div className={`navbarWrapper ${show && "navfadein"}`}>
-      <img className='logo' src={logo} alt=''></img>
-      {!utils.isMobile() &&
-      <div className='navbarLinks'>
-        <div>Mens</div>
-        <div>Womens</div>
-        <div>Kids</div>
-      </div> }
-      <div className='profile'>
-        {utils.isMobile() ? <HamburgerMenu></HamburgerMenu> :  <img className='profilepic' src={profilepic} alt=''></img>}        
+      {utils.isMobile() && <img className="logo" src={logo} alt=""></img>}
+      <div className="wrapper">
+        <img className="logo" src={logo} alt=""></img>
+        {utils.isMobile() && (
+          <HomeOutlinedIcon
+            className="homeicon"
+            fontSize="medium"
+          ></HomeOutlinedIcon>
+        )}
+        {!utils.isMobile() && (
+          <div className="navbarLinks">
+            <div>Mens</div>
+            <div>Womens</div>
+            <div>Kids</div>
+          </div>
+        )}
+        <div className="profile">
+          {utils.isMobile() ? (
+            <HamburgerMenu></HamburgerMenu>
+          ) : (
+            <img className="profilepic" src={profilepic} alt=""></img>
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
