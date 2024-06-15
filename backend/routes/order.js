6629e0e23bd569732c755a49c493d57cd71099d0
Alen Koikkara <74101736+Acoderio1@users.noreply.github.com>
2024-06-15 17:35:56 -0500

    dad

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
-const { placeOrder, placeorder_webhook } = require("../controllers/orderController");
+const { placeOrder } = require("../controllers/orderController");
 const router = express.Router();
 
 router.post("/placeorder/:id", placeOrder);
-router.post("/webhook", express.raw({type: 'application/json'}), placeorder_webhook);
 
 
 module.exports = router;
--- backend/routes/webhooks.js
+++ backend/routes/webhooks.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { placeorder_webhook } = require("../webhooks/webhookController");
 const router = express.Router();
 
-router.post("/place_order", placeorder_webhook);
+router.post("/place_order", express.raw({type: 'application/json'}), placeorder_webhook);
 
 
 module.exports = router;
