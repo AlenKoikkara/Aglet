3a6e27b6ba365d2689b4cf84c15f33506575c1c9
Alen Koikkara <74101736+Acoderio1@users.noreply.github.com>
2024-06-29 19:40:36 -0500

    added try catch for backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,73 +4,90 @@ const Cart = require("../models/cartModel");
 
 const placeOrder = async (req, res) => {
   let customerObj;
+  try {
 
-  const customer = await stripe.customers.search({
-    query: `email:\'${req.body?.emailId}\'`,
-  });
- 
-  const line_items = req.body?.order?.map((item) => {
-    return {
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: item.productName,
-          images: [item.imageUrl],
+    const customer = await stripe.customers.search({
+      query: `email:\'${req.body?.emailId}\'`,
+    });
+   
+    const line_items = req.body?.order?.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.productName,
+            images: [item.imageUrl],
+          },
+          unit_amount: item.listPrice * 100,
         },
-        unit_amount: item.listPrice * 100,
+        quantity: item.count,
+      };
+    });
+  
+    customerObj = {
+      customer_email: req.body?.emailId,
+      line_items,
+      metadata: {
+        customerDb_id: req.body?.userId,
+        cartId: req.body?.order._id
       },
-      quantity: item.count,
+      customer_creation: "always",
+      mode: "payment",
+      success_url: req.body?.success_url,
+      cancel_url: req.body?.cancel_url,
     };
-  });
-
-  customerObj = {
-    customer_email: req.body?.emailId,
-    line_items,
-    metadata: {
-      customerDb_id: req.body?.userId,
-      cartId: req.body?.order._id
-    },
-    customer_creation: "always",
-    mode: "payment",
-    success_url: req.body?.success_url,
-    cancel_url: req.body?.cancel_url,
-  };
-
-  if (customer.data[0]?.id) {
-    customerObj["customer"] = customer.data[0].id;
-    delete customerObj.customer_email;
-    delete customerObj.customer_creation;
+  
+    if (customer.data[0]?.id) {
+      customerObj["customer"] = customer.data[0].id;
+      delete customerObj.customer_email;
+      delete customerObj.customer_creation;
+    }
+    const session = await stripe.checkout.sessions.create(customerObj);
+    res.send({ url: session.url });
   }
-  const session = await stripe.checkout.sessions.create(customerObj);
-  res.send({ url: session.url });
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 };
 
 const addOrder = async (checkoutObject) => {
-  const item = await Cart.find({
-    emailId: checkoutObject.customer_details.email,
-  });
-  const orderObj = {
-    cartId: checkoutObject.metadata.cartId,
-    userId: checkoutObject.metadata.customerDb_id,
-    emailId: checkoutObject.customer_details.email,
-    order: item[0]?.cart
-  }
-  const order = await Order.create(orderObj).catch((error) => {
-    console.log(error.message);
-  }).then(async () => {
-    await Cart.deleteOne( {
+  try {
+    const item = await Cart.find({
       emailId: checkoutObject.customer_details.email,
     });
-  })
+    const orderObj = {
+      cartId: checkoutObject.metadata.cartId,
+      userId: checkoutObject.metadata.customerDb_id,
+      emailId: checkoutObject.customer_details.email,
+      order: item[0]?.cart
+    }
+    const order = await Order.create(orderObj).catch((error) => {
+      console.log(error.message);
+    }).then(async () => {
+      await Cart.deleteOne( {
+        emailId: checkoutObject.customer_details.email,
+      });
+    })
+  }
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 };
 
 const getOrder = async (req, res) => {
-  console.log(req.params.id)
-  const order = await Order.find({ "emailId": req.params.id });
-  if (!order) {
-    return res.status(404).json({ error: "no orderfound" });
+  try {
+    const order = await Order.find({ "emailId": req.params.id });
+    if (!order) {
+      return res.status(404).json({ error: "no orderfound" });
+    }
+    res.status(200).json(order);
   }
-  res.status(200).json(order);
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 };
 
 module.exports = {
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,78 +3,105 @@ const mongoose = require('mongoose')
 
 // get all products
 const getProducts = async (req, res) => {
-  const excludeFields = ["limit", "page", "productCount"];
-  const querObj = {...req?.query};
-  excludeFields.forEach((item) => {
-    delete querObj[item];
-  })
-  const products = await Product.find(querObj).limit(req?.query?.limit || "");
-  if (!products) {
-    return res.status(404).json({ error: "no products" });
+  try {
+    const excludeFields = ["limit", "page", "productCount"];
+    const querObj = {...req?.query};
+    excludeFields.forEach((item) => {
+      delete querObj[item];
+    })
+    const products = await Product.find(querObj).limit(req?.query?.limit || "");
+    if (!products) {
+      return res.status(404).json({ error: "no products" });
+    }
+    res.status(200).json({products:products})
   }
-  res.status(200).json({products:products})
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 }
 
 const getPaginatedProducts = async (req, res) => {
-  const page = parseInt(req?.query?.page)
-  const excludeFields = ["limit", "page", "productCount"];
-  const querObj = {...req?.query};
-  excludeFields.forEach((item) => {
-    delete querObj[item];
-  })
-  const data = await Product.countDocuments(querObj)
-  const start = page * req?.query?.limit
-  const end = start + parseInt(req?.query?.limit)
-  const products = await Product.find(querObj).limit(req?.query?.limit || "").skip(start);
-  const nextPage = end < data ? page + 1 : null
-  if (start > data) {
-    return res.status(404).json({ error: "Invalid Page" });
+  try {
+    const page = parseInt(req?.query?.page)
+    const excludeFields = ["limit", "page", "productCount"];
+    const querObj = {...req?.query};
+    excludeFields.forEach((item) => {
+      delete querObj[item];
+    })
+    const data = await Product.countDocuments(querObj)
+    const start = page * req?.query?.limit
+    const end = start + parseInt(req?.query?.limit)
+    const products = await Product.find(querObj).limit(req?.query?.limit || "").skip(start);
+    const nextPage = end < data ? page + 1 : null
+    if (start > data) {
+      return res.status(404).json({ error: "Invalid Page" });
+    }
+    res.status(200).json({nextPage: nextPage, products:products})
   }
-  res.status(200).json({nextPage: nextPage, products:products})
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 }
 
 // get a single product
 const getProduct = async (req, res) => {
-  const { id } = req.params
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({error: 'No such product'})
+  try {
+    const { id } = req?.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({error: 'No such product'})
+    }
+    const product = await Product.findById({_id: id})
+    if (!product) {
+      return res.status(404).json({error: 'No such product'})
+    }
+    res.status(200).json(product)
   }
-
-  const product = await Product.findById({_id: id})
-
-  if (!product) {
-    return res.status(404).json({error: 'No such product'})
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
   }
-
-  res.status(200).json(product)
 }
 
 const getFeatured = async (req, res) => {
-  const queryObj = req.body
-  const products = await Product.find(queryObj).limit(6)
-  if (!products) {
-    return res.status(404).json({ error: "no products" });
+  try {
+    const queryObj = req.body
+    const products = await Product.find(queryObj).limit(6)
+    if (!products) {
+      return res.status(404).json({ error: "no products" });
+    }
+    res.status(200).json(products)
   }
-  res.status(200).json(products)
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 }
 
 const searchProducts = async (req, res) => {
-  const query = req.query.qs;
-  console.log(req.query?.qs)
-  let products = [];
-  if (query) {
-    products = await Product.aggregate().search({
-      text: {
-        query: query,
-        path: {
-          wildcard: "*",
+  try {
+    const query = req.query.qs;
+    console.log(req.query?.qs)
+    let products = [];
+    if (query) {
+      products = await Product.aggregate().search({
+        text: {
+          query: query,
+          path: {
+            wildcard: "*",
+          },
         },
-      },
-      index: "productSearch",
+        index: "productSearch",
+      }
+    ).limit(5)
     }
-  ).limit(5)
+    res.status(200).json(products)
   }
-  res.status(200).json(products)
+  catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message)
+  }
 }
 
 module.exports = {
